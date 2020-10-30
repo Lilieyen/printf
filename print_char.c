cd42f071ce98c502b64159c8b2f121f2bfc34eca
mosesmrima <mrimamss@gmail.com>
2020-10-30 22:55:42 +0300

    function to print char

---
+++ print_char.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *print_char - prints a character
+ *arg: char to print
+ *return: 1 on success
+ */
+
+int print_char(va_list arg)
+{
+    char c;
+    c = va_arg(arg, int);
+    _putchar(c);
+    return (1);
+}
