92f23b879670d6cf6c42a5c7834e013f34b7b107
lnyasita@gmail.com <Lilieyen>
2020-10-30 19:52:05 +0000

    header file

--- holberton.h
+++ holberton.h
@@ -1,14 +1,17 @@
+/**                                                                                                                                                                     
+ *holberton.h : include file                                                                                                                                            
+ */
+
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdio.h>
-
-int int_len(int number);
-int handle_switch(char format, va_list arguments);
+int _putchar(char c);
+int _strlen(char *s);
+int _printf(const char *format, ...);
+int print_str(va_list arg);
 int print_char(va_list arg);
 int print_number(va_list arg);
-int _printf(const char *format, ...);
-int _putchar(char c);
-
+int handle_switch(char format, va_list arguments);
 #endif
