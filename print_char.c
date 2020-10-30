794d0dcfe95597eae6cfc5505ac9fa3262fbe593
mosesmrima <mrimamss@gmail.com>
2020-10-30 23:24:37 +0300

    prints char

--- print_char.c
+++ print_char.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 /**
  *print_char - prints a character
- *arg: char to print
- *return: 1 on success
+ *@arg: char to print
+ *Return: 1 on success
  */
 
 int print_char(va_list arg)
 {
-    char c;
-    c = va_arg(arg, int);
-    _putchar(c);
-    return (1);
+char c;
+
+c = va_arg(arg, int);
+
+_putchar(c);
+return (1);
 }
